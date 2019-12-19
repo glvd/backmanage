b02fdfac8c05f57e0828fcafd07c623991f94c31
godcong <jumbycc@163.com>
2019-12-19 19:04:08 +0800

    update backend

--- main.go
+++ main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	_ "github.com/glvd/backmanage/adapter/gin"
-	"github.com/glvd/backmanage/echarts"
-	"github.com/glvd/go-admin/template/chartjs"
+	"github.com/glvd/backmanage/datamodel"
+	"github.com/glvd/go-admin/plugins/admin"
 	_ "github.com/glvd/themes/adminlte"
 	_ "github.com/glvd/themes/sword"
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gin-gonic/gin"
-	"github.com/glvd/backmanage/datamodel"
 	"github.com/glvd/backmanage/login"
 	"github.com/glvd/backmanage/pages"
 	"github.com/glvd/go-admin/engine"
 	"github.com/glvd/go-admin/modules/config"
-	"github.com/glvd/go-admin/plugins/admin"
 	"github.com/glvd/go-admin/template"
 	"github.com/glvd/go-admin/template/types"
 	template2 "html/template"
@@ -33,15 +31,14 @@ func main() {
 	//
 	// "user" => http://localhost:9033/admin/info/user
 	//
-	adminPlugin.AddGenerator("user", datamodel.GetUserTable)
+	//adminPlugin.AddGenerator("user", datamodel.GetUserTable)
 
 	template.AddLoginComp(login.GetLoginComponent())
-	template.AddComp(chartjs.NewChart())
-	template.AddComp(echarts.NewChart())
+	//template.AddComp(chartjs.NewChart())
+	//template.AddComp(echarts.NewChart())
 
 	//rootPath := "/data/www/go-admin"
 	rootPath := "."
-
 	cfg := config.ReadFromJson(rootPath + "/config.json")
 	//cfg.CustomFootHtml = template2.HTML(`<div style="display:none;">
 	//<script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1278156902&web_id=1278156902"></script>
@@ -58,33 +55,33 @@ func main() {
 
 	// you can custom your pages like:
 
-	r.GET("/admin", func(ctx *gin.Context) {
-		eng.Content(ctx, func(ctx interface{}) (types.Panel, error) {
-			return pages.GetDashBoard2Content()
-		})
-	})
+	//r.GET("/admin", func(ctx *gin.Context) {
+	//	eng.Content(ctx, func(ctx interface{}) (types.Panel, error) {
+	//		return pages.GetDashBoard2Content()
+	//	})
+	//})
 
-	r.GET("/admin/dash", func(ctx *gin.Context) {
-		eng.Content(ctx, func(ctx interface{}) (panel types.Panel, e error) {
-			return pages.GetDashBoardContent()
-		})
-	})
+	//r.GET("/admin/dash", func(ctx *gin.Context) {
+	//	//	eng.Content(ctx, func(ctx interface{}) (panel types.Panel, e error) {
+	//	//		return pages.GetDashBoardContent()
+	//	//	})
+	//	//})
 	r.GET("/admin/video", func(ctx *gin.Context) {
 		eng.Content(ctx, func(ctx interface{}) (panel types.Panel, e error) {
 			return pages.GetVideoContent()
 		})
 	})
-	r.GET("/admin/form1", func(ctx *gin.Context) {
-		eng.Content(ctx, func(ctx interface{}) (types.Panel, error) {
-			return pages.GetForm1Content()
-		})
-	})
-
-	r.GET("/admin/echarts", func(ctx *gin.Context) {
-		eng.Content(ctx, func(ctx interface{}) (types.Panel, error) {
-			return pages.GetDashBoard3Content()
-		})
-	})
+	//r.GET("/admin/form1", func(ctx *gin.Context) {
+	//	eng.Content(ctx, func(ctx interface{}) (types.Panel, error) {
+	//		return pages.GetForm1Content()
+	//	})
+	//})
+	//
+	//r.GET("/admin/echarts", func(ctx *gin.Context) {
+	//	eng.Content(ctx, func(ctx interface{}) (types.Panel, error) {
+	//		return pages.GetDashBoard3Content()
+	//	})
+	//})
 
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.Redirect(http.StatusMovedPermanently, "/admin")
