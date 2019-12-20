5396c95f96f5e8a5f71e3c3cf40a9c1b2f31059b
godcong <jumbycc@163.com>
2019-12-20 16:21:41 +0800

    update backend

--- datamodel/videos.go
+++ datamodel/videos.go
@@ -5,6 +5,7 @@ import (
 	"github.com/glvd/go-admin/plugins/admin/modules/table"
 	"github.com/glvd/go-admin/template/types"
 	"github.com/glvd/go-admin/template/types/form"
+	uuid "github.com/satori/go.uuid"
 )
 
 // GetVideosTable ...
@@ -15,23 +16,28 @@ func GetVideosTable() (videosTable table.Table) {
 
 	videosTable = table.NewDefaultTable(cfg)
 	info := videosTable.GetInfo()
-	info.AddField("ID", "id", db.Varchar).FieldSortable()
+	info.AddField("ID", "id", db.Int).FieldSortable()
+	info.AddField("VID", "vid", db.Varchar)
 	info.AddField("Poster", "poster", db.Text).FieldDisplay(func(value types.FieldModel) interface{} {
 		if value.Value == "" {
 			return ""
 		}
 		return "<img src=\"/uploads/" + value.Value + "\"/>"
 	})
-	info.AddField("Added", "added", db.Timestamp)
+	info.AddField("VideoInfo", "video_info", db.Text)
+	info.AddField("CreateTime", "created_at", db.Timestamp)
+	info.AddField("UpdateTime", "updated_at", db.Timestamp)
 
 	info.SetTable("videos").SetTitle("Videos").SetDescription("Videos")
 
 	//edit/add form
 	formList := videosTable.GetForm()
-	formList.AddField("ID", "id", db.Varchar, form.Text).FieldNotAllowEdit().FieldNotAllowAdd()
+	formList.AddField("ID", "id", db.Int, form.Default).FieldNotAllowEdit().FieldNotAllowAdd()
+	formList.AddField("VID", "vid", db.Varchar, form.Text).FieldNotAllowEdit().FieldDefault(uuid.NewV1().String())
 	formList.AddField("Poster", "poster", db.Text, form.File)
-	formList.AddField("Added", "added", db.Timestamp, form.Datetime)
-
+	formList.AddField("VideoInfo", "video_info", db.Text, form.TextArea)
+	formList.AddField("CreateTime", "created_at", db.Timestamp, form.Datetime).FieldNotAllowEdit().FieldNotAllowAdd()
+	formList.AddField("UpdateTime", "updated_at", db.Timestamp, form.Datetime).FieldNotAllowEdit().FieldNotAllowAdd()
 	formList.SetTable("videos").SetTitle("Videos").SetDescription("Videos")
 	return
 }
