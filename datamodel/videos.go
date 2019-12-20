b9278baeb3a0ae89038f548cabe65dd4a0e45e89
godcong <jumbycc@163.com>
2019-12-20 16:02:31 +0800

    update backend

--- datamodel/videos.go
+++ datamodel/videos.go
@@ -5,13 +5,15 @@ import (
 	"github.com/glvd/go-admin/plugins/admin/modules/table"
 	"github.com/glvd/go-admin/template/types"
 	"github.com/glvd/go-admin/template/types/form"
-	uuid "github.com/satori/go.uuid"
 )
 
 // GetVideosTable ...
 func GetVideosTable() (videosTable table.Table) {
-	videosTable = table.NewDefaultTable(table.DefaultConfig())
+	cfg := table.DefaultConfig()
+	//cfg.PrimaryKey.Type = db.Varchar
+	//cfg.PrimaryKey.Name = "id"
 
+	videosTable = table.NewDefaultTable(cfg)
 	info := videosTable.GetInfo()
 	info.AddField("ID", "id", db.Varchar).FieldSortable()
 	info.AddField("Poster", "poster", db.Text).FieldDisplay(func(value types.FieldModel) interface{} {
@@ -26,11 +28,7 @@ func GetVideosTable() (videosTable table.Table) {
 
 	//edit/add form
 	formList := videosTable.GetForm()
-	formList.AddField("ID", "id", db.Varchar, form.Text).FieldNotAllowEdit().FieldHide().FieldDefault(uuid.NewV1().String())
-	//.FieldPostFilterFn(
-	//	func(value types.PostFieldModel) string {
-	//		return uuid.NewV1().String()
-	//	})
+	formList.AddField("ID", "id", db.Varchar, form.Text).FieldNotAllowEdit().FieldNotAllowAdd()
 	formList.AddField("Poster", "poster", db.Text, form.File)
 	formList.AddField("Added", "added", db.Timestamp, form.Datetime)
 
