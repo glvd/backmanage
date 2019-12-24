c88436632c733ca7213f7d674e73792a31f67efd
godcong <jumbycc@163.com>
2019-12-24 18:51:50 +0800

    update backend

--- datamodel/videos.go
+++ datamodel/videos.go
@@ -2,11 +2,8 @@ package datamodel
 
 import (
 	"github.com/glvd/go-admin/modules/db"
-	form2 "github.com/glvd/go-admin/plugins/admin/modules/form"
 	"github.com/glvd/go-admin/plugins/admin/modules/table"
 	"github.com/glvd/go-admin/template/types"
-	"github.com/glvd/go-admin/template/types/form"
-	"github.com/goextension/log"
 )
 
 // GetVideosTable ...
@@ -14,7 +11,21 @@ func GetVideosTable() (videosTable table.Table) {
 	cfg := table.DefaultConfig()
 	//cfg.PrimaryKey.Type = db.Varchar
 	//cfg.PrimaryKey.Name = "id"
-	videosTable = table.NewDefaultTable(cfg)
+	cfg.Editable = false
+	cfg.CanAdd = false
+	cfg.Deletable = false
+	videosTable = table.NewDefaultTable(table.Config{
+		Driver:     db.DriverMysql,
+		CanAdd:     false,
+		Editable:   false,
+		Deletable:  false,
+		Exportable: true,
+		Connection: table.DefaultConnectionName,
+		PrimaryKey: table.PrimaryKey{
+			Type: db.Int,
+			Name: table.DefaultPrimaryKeyName,
+		},
+	})
 	info := videosTable.GetInfo()
 	info.AddField("ID", "id", db.Int).FieldSortable()
 	info.AddField("Poster", "poster", db.Text).FieldDisplay(func(value types.FieldModel) interface{} {
@@ -32,17 +43,17 @@ func GetVideosTable() (videosTable table.Table) {
 
 	//edit/add form
 	formList := videosTable.GetForm()
-	formList.SetBeforeUpdate(func(values form2.Values) error {
-		log.Infow("update", "poster", values.Get("poster"))
-		if poster := values.Get("poster"); poster == "" {
-			values.Delete("poster")
-		}
-		return nil
-	})
-	formList.AddField("ID", "id", db.Int, form.Default).FieldNotAllowAdd().FieldNotAllowEdit()
-	formList.AddField("Poster", "poster", db.Text, form.Text)
-	formList.AddField("VideoNo", "video_no", db.Varchar, form.Text)
-
+	//formList.SetBeforeUpdate(func(values form2.Values) error {
+	//	log.Infow("update", "poster", values.Get("poster"))
+	//	if poster := values.Get("poster"); poster == "" {
+	//		values.Delete("poster")
+	//	}
+	//	return nil
+	//})
+	//formList.AddField("ID", "id", db.Int, form.Default).FieldNotAllowAdd().FieldNotAllowEdit()
+	//formList.AddField("Poster", "poster", db.Text, form.Text)
+	//formList.AddField("VideoNo", "video_no", db.Varchar, form.Text)
+	//
 	formList.SetTable("videos").SetTitle("Videos").SetDescription("Videos")
 	return
 }
