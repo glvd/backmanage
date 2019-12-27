2ff52e7dd474a1ab4562c726e42a93eab3dd2a60
godcong <jumbycc@163.com>
2019-12-27 14:43:55 +0800

    update backend

--- model/video.go
+++ model/video.go
@@ -68,6 +68,40 @@ type Video struct {
 	Uncensored   bool      `gorm:"uncensored" json:"uncensored"`                                //有码,无码
 }
 
+// OnlineVideoJSON ...
+type OnlineVideoJSON struct {
+	No           string   `json:"no"`            //编号
+	Intro        string   `json:"intro"`         //简介
+	Alias        []string `json:"alias"`         //别名，片名
+	ThumbHash    string   `json:"thumb_hash"`    //缩略图
+	PosterHash   string   `json:"poster_hash"`   //海报地址
+	SourceHash   string   `json:"source_hash"`   //原片地址
+	M3U8Hash     string   `json:"m3u8_hash"`     //切片地址
+	Key          string   `json:"-"`             //秘钥
+	M3U8         string   `json:"-"`             //M3U8名
+	Roles        []string `json:"role"`          //主演
+	Director     string   `json:"director"`      //导演
+	Systematics  string   `json:"systematics"`   //分级
+	Season       string   `json:"season"`        //季
+	TotalEpisode string   `json:"total_episode"` //总集数
+	Episode      string   `json:"episode"`       //集数
+	Producer     string   `json:"producer"`      //生产商
+	Publisher    string   `json:"publisher"`     //发行商
+	Type         string   `json:"type"`          //类型：film，FanDrama
+	Format       string   `json:"format"`        //输出格式：3D，2D,VR(VR格式：Half-SBS：左右半宽,Half-OU：上下半高,SBS：左右全宽)
+	Language     string   `json:"language"`      //语言
+	Caption      string   `json:"caption"`       //字幕
+	Group        string   `json:"group"`         //分组
+	Index        string   `json:"index"`         //索引
+	Date         string   `json:"date"`          //发行日期
+	Sharpness    string   `json:"sharpness"`     //清晰度
+	Series       string   `json:"series"`        //系列
+	Tags         []string `json:"tags"`          //标签
+	Length       string   `json:"length"`        //时长
+	Sample       []string `json:"sample"`        //样板图
+	Uncensored   bool     `json:"uncensored"`    //有码,无码
+}
+
 func init() {
 	RegisterTable(Video{}, Sample{}, Tag{}, Role{}, Alias{})
 }
