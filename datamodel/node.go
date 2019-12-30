e276795c8aa1543f5eb0e0a6d7ec40228d3c1ddd
godcong <jumbycc@163.com>
2019-12-30 13:55:59 +0800

    update backend

--- datamodel/node.go
+++ datamodel/node.go
@@ -22,15 +22,15 @@ func GetNodeTable() (nodeTable table.Table) {
 	info.AddField("NodeID", "node_id", db.Varchar)
 	info.AddField("NodeAddr", "node_addr", db.Text)
 	info.AddField("SyncData", "sync", db.Tinyint).FieldDisplay(func(value types.FieldModel) interface{} {
-		if value.Value == "0" {
-			return "async"
+		if value.Value == "1" {
+			return "sync"
 		}
-		return "sync"
+		return "async"
 	}).FieldEditAble(editType.Select).FieldEditOptions([]map[string]string{
-		{"value": "0", "text": "async"},
+		{"value": "2", "text": "async"},
 		{"value": "1", "text": "sync"},
 	}).FieldFilterable(types.FilterType{FormType: form.SelectSingle}).FieldFilterOptions([]map[string]string{
-		{"value": "0", "field": "async"},
+		{"value": "2", "field": "async"},
 		{"value": "1", "field": "sync"},
 	}) //.FieldFilterOptionExt(map[string]interface{}{"allowClear": true})
 	info.AddField("NodeStatus", "node_status", db.Int).FieldDisplay(func(value types.FieldModel) interface{} {
@@ -55,18 +55,18 @@ func GetNodeTable() (nodeTable table.Table) {
 	formList.AddField("NodeID", "node_id", db.Varchar, form.Text).FieldNotAllowAdd().FieldNotAllowEdit()
 	//formList.AddField("NodeStatus", "node_status", db.Int, form.Text).FieldNotAllowAdd().FieldNotAllowEdit()
 	formList.AddField("Interval", "interval", db.Int, form.Text).FieldDefault("3")
-	formList.AddField("SyncData", "sync", db.Tinyint, form.Radio).
+	formList.AddField("SyncData", "sync", db.Tinyint, form.Radio).FieldDefault("2").
 		FieldOptions([]map[string]string{
 			{
 				"field":    "sync",
 				"label":    "同步",
 				"value":    "1",
-				"selected": "checked",
+				"selected": "",
 			}, {
 				"field":    "sync",
 				"label":    "非同步",
-				"value":    "0",
-				"selected": "",
+				"value":    "2",
+				"selected": "checked",
 			},
 		})
 	//formList.AddField("CreateTime", "created_at", db.Timestamp, form.Datetime).FieldNotAllowAdd().FieldNotAllowEdit().FieldHide()
@@ -81,19 +81,25 @@ func NodeInfo(values form2.Values) error {
 	if addr2 := values.Get("node_addr"); addr2 != "" {
 		addr = addr2
 	}
+	if values.Get("sync") == "" {
+		values.Add("sync", "2")
+	}
 	values.Add("node_addr", addr)
 	n, e := node.NewSingleNode(addr)
 	if e != nil {
+		values.Add("node_id", "abnormal")
 		values.Add("node_status", "0")
 		return nil
 	}
 
 	if n.ID() == nil {
+		values.Add("node_id", "abnormal")
 		values.Add("node_status", "0")
 		return nil
 	}
 	values.Add("node_id", n.ID().ID)
 	node.AddNode(addr, n)
 	values.Add("node_status", "1")
+
 	return nil
 }
