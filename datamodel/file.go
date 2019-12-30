b877846559e8a3a585721b99c3de67efc384a836
godcong <jumbycc@163.com>
2019-12-30 17:10:19 +0800

    update backend

--- datamodel/file.go
+++ datamodel/file.go
@@ -1,12 +1,10 @@
 package datamodel
 
 import (
-	"github.com/glvd/backmanage/data"
 	"github.com/glvd/go-admin/modules/db"
 	"github.com/glvd/go-admin/plugins/admin/modules/table"
 	"github.com/glvd/go-admin/template/types"
 	"github.com/glvd/go-admin/template/types/form"
-	editType "github.com/glvd/go-admin/template/types/table"
 )
 
 // FileTable ...
@@ -25,26 +23,12 @@ func FileTable() (fTable table.Table) {
 	})
 	info := fTable.GetInfo()
 	info.AddField("ID", "id", db.Varchar).FieldSortable()
-	info.AddField("Poster", "poster_path", db.Text).FieldDisplay(func(value types.FieldModel) interface{} {
-		if value.Value == "" {
-			return ""
-		}
-
-		img := data.ImageLoad(value.Value)
-		if img != "" {
-			img = "data:image/jpg;base64," + img
-		}
-		return "<img height=\"120px\" src=\"" + img + "\"/>"
-	})
-
-	info.AddField("VideoID", "video_id", db.Varchar).FieldSortable().FieldEditAble(editType.Text).FieldFilterable(types.FilterType{Operator: types.FilterOperatorLike})
-	info.AddField("Address", "address", db.Varchar)
-	info.AddField("Progress", "progress", db.Varchar)
-	info.AddField("Status", "status", db.Varchar)
+	info.AddField("Address", "address", db.Text)
+	info.AddField("Name", "name", db.Text)
 	info.AddField("CreateTime", "created_at", db.Timestamp).FieldFilterable(types.FilterType{FormType: form.Datetime})
 	info.AddField("UpdateTime", "updated_at", db.Timestamp)
 
-	info.SetTable("videos").SetTitle("Videos").SetDescription("Videos")
+	info.SetTable("file").SetTitle("Files").SetDescription("Files")
 
 	//fTable.GetInfo().SetTabGroups(types.
 	//	NewTabGroups("video_no", "intro", "created_at").
@@ -52,16 +36,15 @@ func FileTable() (fTable table.Table) {
 	//	SetTabHeaders("profile1", "profile2")
 	//edit/add form
 	formList := fTable.GetForm()
-	formList.SetBeforeInsert(VideoInsert)
 
-	formList.AddField("VideoNo", "video_no", db.Varchar, form.Text).FieldNotAllowEdit()
+	formList.AddField("Address", "address", db.Varchar, form.Text).FieldNotAllowEdit()
 	//formList.AddField("PosterPath", "poster_path", db.Varchar, form.Text)
 	//formList.AddField("ThumbPath", "thumb_path", db.Varchar, form.Text)
-	formList.AddField("SourcePath", "source_path", db.Varchar, form.Text)
+	formList.AddField("Name", "name", db.Varchar, form.Text)
 	//fTable.GetInfo().SetTabGroups(types.
 	//	NewTabGroups("video_no", "intro", "created_at").
 	//	AddGroup("source_path", "tags", "actors"))
 	//SetTabHeaders("profile1", "profile2")
-	formList.SetTable("videos").SetTitle("Videos").SetDescription("Videos")
+	formList.SetTable("file").SetTitle("Files").SetDescription("Files")
 	return
 }
