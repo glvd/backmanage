f5d872b183f4727e05c989ffe5ddaec0a4d62310
godcong <jumbycc@163.com>
2020-01-06 16:58:54 +0800

    update backend

--- modules/scrape/scrape.go
+++ modules/scrape/scrape.go
@@ -27,29 +27,40 @@ func defaultOption() Option {
 		Proxy:    "socks5://127.0.0.1:1080",
 		Optimize: true,
 		Exact:    true,
-		Output:   filepath.Join("data", "info"),
+		Output:   filepath.Join("uploads", "info"),
 	}
 }
 
 // NewScrape ...
-func NewScrape() scrape.IScrape {
+func NewScrape() (s scrape.IScrape) {
 	once.Do(func() {
-		if DefaultOption.Proxy != "" {
-			scrape.RegisterProxy(DefaultOption.Proxy)
-		}
-
-		if DefaultOption.Output != "" {
-			scrape.DefaultOutputPath = DefaultOption.Output
-		}
-
-		Scrape = scrape.NewScrape(
-			scrape.GrabOption(scrape.NewGrabJavbus()),
-			scrape.GrabOption(scrape.NewGrabJavdb()),
-			scrape.OptimizeOption(DefaultOption.Optimize),
-			scrape.ExactOption(DefaultOption.Exact),
-		)
+		s = newScrape()
 	})
+	return
+}
+
+// NewScrape ...
+func newScrape() scrape.IScrape {
+	if DefaultOption.Proxy != "" {
+		scrape.RegisterProxy(DefaultOption.Proxy)
+	}
+
+	if DefaultOption.Output != "" {
+		scrape.DefaultOutputPath = DefaultOption.Output
+	}
+
+	Scrape = scrape.NewScrape(
+		scrape.GrabOption(scrape.NewGrabJavbus()),
+		scrape.GrabOption(scrape.NewGrabJavdb()),
+		scrape.OptimizeOption(DefaultOption.Optimize),
+		scrape.ExactOption(DefaultOption.Exact),
+	)
 	return Scrape
+}
+
+// RenewScrape ...
+func RenewScrape() scrape.IScrape {
+	return newScrape()
 }
 
 // FindContent ...
