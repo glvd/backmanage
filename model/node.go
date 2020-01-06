018ed3b148ae58854693fe6278792c5e1a4a1452
godcong <jumbycc@163.com>
2020-01-06 17:43:59 +0800

    update backend

--- model/node.go
+++ model/node.go
@@ -1,5 +1,14 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 // Node ...
 type Node struct {
+	gorm.Model
+	Sync       int
+	NodeType   string
+	NodeID     string
+	NodeStatus int
+	NodeAddr   string
+	Interval   int
 }
