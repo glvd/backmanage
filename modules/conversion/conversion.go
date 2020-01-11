8bce7e787a294da158214638fddb7c06e0a73bb8
godcong <jumbycc@163.com>
2020-01-11 23:17:23 +0800

    update backend

--- modules/conversion/conversion.go
+++ modules/conversion/conversion.go
@@ -1,6 +1,15 @@
 package conversion
 
+import (
+	"github.com/glvd/cup/config"
+	"github.com/glvd/cup/service"
+)
+
+var _service *service.Service
+
 // Start ...
 func Start() {
+	cfg := config.Default()
 
+	_service = service.NewService(*cfg)
 }
